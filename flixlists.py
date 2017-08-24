845128a46637bab6e8d0c031c916390ddf22d529
Mark <spiffylogic@gmail.com>
2017-08-23 20:52:15 -0400

    Remove cookie option from main program

--- flixlists.py
+++ flixlists.py
@@ -18,9 +18,6 @@ elif sys.argv[1] == 'netflix-search' and len(sys.argv) > 2:
     title = sys.argv[2]
     print 'Is ' + title + ' available on Netflix?'
     print netflix.isAvailable(title)
-elif sys.argv[1] == 'netflix-cookie':
-    print 'Paste Netflix cookie here:'
-    # TODO: save pasted cookie
 elif sys.argv[1] == 'imdb':
     print 'Show IMDb Watchlist'
 else:
